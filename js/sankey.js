f6be97ff2709280dfb723f96a90f96ae3a0ad304
Maxime PYZ <maxime.pyz@etu.umontpellier.fr>
2017-05-16 09:51:38 +0200

    ajout de link par pattern, il faudra refactorer le code + corriger certaines erreurs

--- js/sankey.js
+++ js/sankey.js
@@ -53,14 +53,15 @@ d3.sankey = function() {
   sankey.link = function() {
     var curvature = .5;
  
-    function link(d) {
+    function link(d, nb) {
       var x0 = d.source.x + d.source.dx,
           x1 = d.target.x,
           xi = d3.interpolateNumber(x0, x1),
           x2 = xi(curvature),
           x3 = xi(1 - curvature),
-          y0 = d.source.y + d.sy + d.dy / 2,
-          y1 = d.target.y + d.ty + d.dy / 2;
+          y0 =(nb==null)? 0 : nb + d.source.y + d.sy + d.dy / 2,
+          y1 =(nb==null)? 0 : nb + d.target.y + d.ty + d.dy / 2;
+      //console.log(y0 + " // " + y1 + " // nb : " + nb);
       return "M" + x0 + "," + y0
            + "C" + x2 + "," + y0
            + " " + x3 + "," + y1
